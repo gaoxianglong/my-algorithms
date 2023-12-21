1f88a23fb97a7ded22c32b5af420fd2c9900b8b5
JohnGao <johngao@JohnGaodeMacBook-Pro.local>
2023-12-21 13:51:05 +0800

    每日一题

--- src/main/java/com/gxl/algorithm/easy/ClimbingStairs.java
+++ src/main/java/com/gxl/algorithm/easy/ClimbingStairs.java
@@ -17,10 +17,11 @@ public class ClimbingStairs {
             return 1;
         }
         var dp = new int[n + 1];
+        // 只有一种方式到达0阶和1阶
         dp[0] = 1;
         dp[1] = 1;
         for (var i = 2; i <= n; i++) {
-            // 状态转移方程dp[i-1] + dp[i-2]
+            // 状态转移方程为dp[i-1] + dp[i-2]
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
