d970794e199d7bc68006f9ddcbd679ca7a8de5bd
JohnGao <johngao@JohnGaodeMacBook-Pro.local>
2023-12-25 21:24:41 +0800

    每日一题

--- src/main/java/com/gxl/algorithm/medium/JumpGame2.java
+++ src/main/java/com/gxl/algorithm/medium/JumpGame2.java
@@ -13,12 +13,15 @@ import org.junit.Assert;
  */
 public class JumpGame2 {
     public int jump(int[] nums) {
-        int count = 0, max = 0, j = 0;
-        for (var i = 0; i < nums.length - 1; i++) {
-            max = Math.max(max, i + nums[i]);
-            if (i == j) {
-                j = max;
+        int count = 0, max = 0, end = 0, n = nums.length - 1;
+        for (var i = 0; i < n; i++) {
+            max = Math.max(max, nums[i] + i);
+            if (i == end) {
+                end = max;
                 count++;
+                if (end > n) {
+                    break;
+                }
             }
         }
         return count;
